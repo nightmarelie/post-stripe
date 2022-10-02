b836818e03301b23b6de78b93f90a91e69f26a74
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-10-01 20:05:05 -0600

    feat: add stripe provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { StripeProvider, Elements } from "react-stripe-elements";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <StripeProvider apiKey="test">
+      <Elements>
+        <App />
+      </Elements>
+    </StripeProvider>
   </React.StrictMode>
 );
 
